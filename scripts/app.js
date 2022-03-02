0df697622e65c7ef0b913193357365d0e850587f
Timo <timo.angst@hli.ch>
2022-03-02 08:07:47 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 window.addEventListener("load", function(event) {
-  calcLandingPage()
+  calcPages()
 })
 
 window.addEventListener("resize", function(even){
-  calcLandingPage()
+  calcPages()
 })
 
-function calcLandingPage() {
+function calcPages() {
   let header = document.querySelector(".header")
-  let landingPage = document.querySelector("#main")
-  landingPage.style.height = (window.innerHeight - header.offsetHeight + 0.5) + "px"
+  let pages = document.querySelectorAll(".pages")
+
+  pages.forEach(page => {
+    page.style.height = window.innerHeight - header.offsetHeight + 0.5 + "px"
+    page.style.paddingTop = header.offsetHeight + 0.5 + "px"
+    page.style.paddingBottom = header.offsetHeight + 0.5 + "px"
+  })
 }
+
