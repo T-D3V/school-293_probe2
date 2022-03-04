3920f396f81601a91b8b2327dca88d42f686135b
Timo Angst <timo.angst@bluewin.ch>
2022-03-04 11:13:12 +0100

    responsive

--- js/app.js
+++ js/app.js
@@ -21,3 +21,14 @@ function calcLayout() {
   })
 }
 
+function mobileNav() {
+  let nav = document.querySelector("nav");
+  if(window.outerWidth <= 675){
+    if(!nav.classList.contains("active")){
+      nav.classList.add("active")
+    }else {
+      nav.classList.remove("active")
+    }
+  }
+}
+
